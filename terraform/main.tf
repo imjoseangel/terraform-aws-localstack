acd1b71215e2929682e52c3c57fcfe2c64e820d0
imjoseangel <josea.munoz@gmail.com>
2021-07-11 11:34:28 +0200

    enable pitr

--- terraform/main.tf
+++ terraform/main.tf
@@ -41,6 +41,10 @@ resource "aws_dynamodb_table" "main" {
   read_capacity          = 1
   write_capacity         = 1
 
+  point_in_time_recovery {
+      enabled = true
+  }
+
   attribute {
     name = "id"
     type = "S"
