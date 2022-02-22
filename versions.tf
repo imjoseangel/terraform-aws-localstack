398d2a2c11b6666f6d120622f1ec0ffb3e58d1d4
imjoseangel <josea.munoz@gmail.com>
2022-02-22 22:15:40 +0100

    add endpoints

--- versions.tf
+++ versions.tf
@@ -2,12 +2,20 @@ terraform {
   required_version = ">= 1.0"
 
   required_providers {
-    aws = ">= 3.24"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.2.0"
+    }
   }
 }
 
 provider "aws" {
-  region                      = local.region
+
+  access_key = "mock_access_key"
+  secret_key = "mock_secret_key"
+  region     = local.region
+
+  s3_force_path_style         = true
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
@@ -18,6 +26,6 @@ provider "aws" {
     iam             = "http://localhost:4566"
     networkfirewall = "http://localhost:4566"
     networkmanager  = "http://localhost:4566"
-
+    s3              = "http://localhost:4566"
   }
 }
