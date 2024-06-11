c91c27cab02d4d3304efb2c736b4a1c91a9e08cb
imjoseangel <josea.munoz@gmail.com>
2024-06-11 23:22:45 +0200

    add fixes

--- main.tf
+++ main.tf
@@ -80,6 +80,8 @@ resource "aws_security_group" "allow_icmp" {
     description = "ICMP from VPC"
     protocol    = "icmp"
     cidr_blocks = [aws_vpc.main.cidr_block]
+    from_port   = 80
+    to_port     = 80
   }
 
   tags = {
